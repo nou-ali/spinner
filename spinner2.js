21da4cc7cf5f7e9383f08e38d62c2e284ea405d5
nou-ali <noura4403@gmail.com>
2021-11-30 14:14:00 +0000

    refactored spinner

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+
+let speed = 100;
+
+let spinner = ['\r|   ','\r/   ','\r-   ','\r\\   ','\r|   ','\r~   ','\r*   '];
+
+for (const spin of spinner) {
+  setTimeout(() => {
+    process.stdout.write(spin);
+}, speed) 
+  speed += 150; 
+};
+
+
+
